52a239aab11fbe266ad7d681eb3d82ef7dd2fa37
katyjane8 <katy.jane8@gmail.com>
2017-08-22 13:16:04 -0600

    unicorn.rb

---
+++ mythical-creatures/unicorn.rb
@@ -0,0 +1,27 @@
+class Unicorn
+  def initialize(name, color = "white")
+    @name = name
+    @color = color
+  end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def white?
+    if color == "white"
+      true
+    else
+      false
+    end
+  end
+
+  def say(phrase)
+    "**;* #{phrase} **;*"
+  end
+
+end
